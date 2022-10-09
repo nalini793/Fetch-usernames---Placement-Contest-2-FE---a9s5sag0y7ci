8ca634bc652923b23630d01013b357c68e253f87
nalini793 <97171653+nalini793@users.noreply.github.com>
2022-10-09 14:59:55 +0530

    edit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
-import '../styles/App.css';
-import { useState, useEffect } from 'react';
+import React from "react";
+import "../styles/App.css";
+import { useState, useEffect } from "react";
 const App = () => {
-//code here 
- 
+  //code here
+  const [name, setName] = useState("");
+  const [user, setUser] = useState([]);
+  const url = "https://content.newtonschool.co/v1/pr/main/users";
 
-
+  function changeInput(e) {
+    const id = e.target.value;
+    setName(user[id - 1].name);
+  }
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+      });
+  }, []);
 
   return (
     <div className="App">
@@ -14,7 +26,6 @@ const App = () => {
       <p id="name">{name}</p>
     </div>
   );
-}
-
+};
 
 export default App;
